0b987480a3ad924157910ba88a40355bb595c537
Stephan Ullmann <stephan.ullmann@posteo.de>
2024-01-30 09:16:57 +0100

    removed commas!

--- myFunctions.js
+++ myFunctions.js
@@ -5,7 +5,7 @@ export function sayHello1(name) {
    * Take a paramenter called name and return a string that
    * e.g. "Hello, John"
    * */
-  return `Hello, ${name}`;
+  return `Hello ${name}`;
 }
 
 // ES6
@@ -15,5 +15,5 @@ export const sayHello2 = (name, timeOfDay) => {
    * Take two paramenter one name and second one called timeOfDay (morning, evening, night) return a string that greets the name at that time of day
    * e.g. with name="John" and timeOfDay="morning", return "Good morning, John". The COMMA IS IMPORTANT!
    * */
-  return `Good ${timeOfDay}, ${name}`;
+  return `Good ${timeOfDay} ${name}`;
 };
